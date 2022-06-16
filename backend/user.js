82b0404236dc58e6a6e4afa7157b125052fecb76
Dhruvi <dhruvisavaliya216@gmail.com>
2022-06-16 10:25:54 +0530

    init commit

---
+++ backend/user.js
@@ -0,0 +1,16 @@
+const data = {
+  users: [
+    {
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: 'Admin@1234',
+      isAdmin: true,
+    },
+    {
+      name: 'Sam',
+      email: 'sam@example.com',
+      password: 'Sam@1234',
+      isAdmin: false,
+    },
+  ],
+};
